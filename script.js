ba803ad1ade02280c2b7df7da4331a6141619f7b
ascurry12 <aniyahcurry12@gmail.com>
2023-01-19 10:16:35 -0600

    fixed semicolons

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function showSlides() {
   let slides = document.getElementsByClassName("images");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
+  };
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex > slides.length) {slideIndex = 1};
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
